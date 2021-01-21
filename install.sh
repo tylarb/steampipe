1b9851839ab88c12536832e82d7ea62551985116
Binaek Sarkar <binaek@turbot.com>
2021-01-21 22:35:47 +0530

    indentation to spaces

--- install.sh
+++ install.sh
@@ -4,30 +4,30 @@
 set -e
 
 if ! command -v unzip >/dev/null; then
-	echo "Error: `unzip` is required to install Steampipe." 1>&2
-	exit 1
+    echo "Error: `unzip` is required to install Steampipe." 1>&2
+    exit 1
 fi
 
 if ! command -v install >/dev/null; then
-	echo "Error: `install` is required to install Steampipe." 1>&2
-	exit 1
+    echo "Error: `install` is required to install Steampipe." 1>&2
+    exit 1
 fi
 
 if [ "$OS" = "Windows_NT" ]; then
-	echo "Error: Windows is not supported yet." 1>&2
-	exit 1
+    echo "Error: Windows is not supported yet." 1>&2
+    exit 1
 else
-	case $(uname -sm) in
-	"Darwin x86_64") target="darwin_amd64" ;;
-	"Darwin arm64") echo "Error: ARM is not supported yet." 1>&2;exit 1 ;;
-	*) target="linux_amd64" ;;
-	esac
+    case $(uname -sm) in
+    "Darwin x86_64") target="darwin_amd64" ;;
+    "Darwin arm64") echo "Error: ARM is not supported yet." 1>&2;exit 1 ;;
+    *) target="linux_amd64" ;;
+    esac
 fi
 
 if [ $# -eq 0 ]; then
-	steampipe_uri="https://github.com/turbot/steampipe/releases/latest/download/steampipe_${target}.zip"
+    steampipe_uri="https://github.com/turbot/steampipe/releases/latest/download/steampipe_${target}.zip"
 else
-	steampipe_uri="https://github.com/turbot/steampipe/releases/download/${1}/steampipe_${target}.zip"
+    steampipe_uri="https://github.com/turbot/steampipe/releases/download/${1}/steampipe_${target}.zip"
 fi
 
 bin_dir="/usr/local/bin"
@@ -40,7 +40,7 @@ zip_location="$tmp_dir/steampipe.zip"
 
 echo "Downloading from $steampipe_uri"
 if command -v wget >/dev/null; then
-	if ! wget -q --show-progress -O "$zip_location" "$steampipe_uri"; then
+    if ! wget -q --show-progress -O "$zip_location" "$steampipe_uri"; then
         echo "Could not find version $1"
         exit 1
     fi
@@ -65,7 +65,7 @@ rm "$zip_location"
 
 echo "Steampipe was installed successfully to $exe"
 if command -v steampipe >/dev/null; then
-	echo "Run 'steampipe --help' to get started"
+    echo "Run 'steampipe --help' to get started"
 else
     echo "Steampipe was installed, but could not be located. Are you sure `/use/local/bin` is exported?"
 fi
